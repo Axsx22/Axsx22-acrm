bf642ae864fc4d9f12c78ce4de6135da33baddc5
Ali Farahani <153266633+Axsx22@users.noreply.github.com>
2026-08-31 10:43:27 +0330

    feat: add Session 30 evolution package

---
+++ acrm_core/evolution/__init__.py
@@ -0,0 +1,19 @@
+"""Controlled ACRM self-evolution components."""
+
+from .session30 import (
+    EvolutionCandidate,
+    EvolutionDecision,
+    EvolutionObservation,
+    EvolutionSession30,
+    EvolutionTolerance,
+    SpecialistVote,
+)
+
+__all__ = [
+    "EvolutionCandidate",
+    "EvolutionDecision",
+    "EvolutionObservation",
+    "EvolutionSession30",
+    "EvolutionTolerance",
+    "SpecialistVote",
+]
